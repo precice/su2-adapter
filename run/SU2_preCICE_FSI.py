0af8dbf32b2fa91e74d4c5135672ad1d7ebf81b2
Joseph Signorelli <jms26@illinois.edu>
2024-09-05 17:21:34 -0500

    Fix control parameters update

--- run/SU2_preCICE_FSI.py
+++ run/SU2_preCICE_FSI.py
@@ -211,10 +211,6 @@ def main():
         # Monitor the solver
         stopCalc = SU2Driver.Monitor(TimeIter)
 
-        # Update control parameters
-        TimeIter += 1
-        time += deltaT
-
         # Loop over the vertices
         for i, iVertex in enumerate(iVertices_MovingMarker_PHYS):
             # Get forces at each vertex
@@ -237,6 +233,9 @@ def main():
             SU2Driver.Output(TimeIter)
             if (stopCalc == True):
                 break
+            # Update control parameters
+            TimeIter += 1
+            time += deltaT
 
         if options.with_MPI == True:
             comm.Barrier()
